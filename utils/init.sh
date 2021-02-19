b5a55a2f8f3d4b8d1d1117fb1d9088fd7e56d833
GuilleSf <guillermonietob@hotmail.com>
2021-02-19 16:30:00 +0100

    Change some utils

--- utils/init.sh
+++ utils/init.sh
@@ -1,9 +1,21 @@
-if [ ! -d "venv" ]; then
+cd ..
+if [ ! -d "env" ]; then
     echo --------------------
     echo Creating virtualenv
     echo --------------------
     python3.9 -m venv env
 fi
-source venv/bin/activate
-
+cd env
+if [ ! -d "env/bin"]; then
+	echo --------------------
+	echo env/bin
+	echo --------------------
+	source bin/activate
+else
+	echo --------------------
+	echo env/Scripts
+	echo --------------------
+	source Scripts/activate
+fi
+cd ..
 pip install -r requirements.txt
