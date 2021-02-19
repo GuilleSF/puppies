fd9d70e10752299ab230f4461c79614f5582da44
GuilleSf <guillermonietob@hotmail.com>
2021-02-20 00:35:32 +0100

    HotFix, maybe we shouldn commit inte dev before the merge

---
+++ utils/merge_feature.sh
@@ -0,0 +1,16 @@
+echo --------------------
+echo Git status
+echo --------------------
+git status
+echo --------------------
+echo Git add
+echo --------------------
+git add --all
+echo --------------------
+echo Git commit
+echo --------------------
+git commit -a
+echo --------------------
+echo Git merge feature into dev
+echo --------------------
+git merge fetature_test
