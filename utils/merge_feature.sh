9d98c2c349d5907f385d055a829250aed17602fe
GuilleSf <guillermonietob@hotmail.com>
2021-02-20 01:10:45 +0100

    Changed merged util

--- utils/merge_feature.sh
+++ utils/merge_feature.sh
@@ -1,16 +1,5 @@
-echo --------------------
-echo Git status
-echo --------------------
-git status
-echo --------------------
-echo Git add
-echo --------------------
-git add --all
-echo --------------------
-echo Git commit
-echo --------------------
-git commit -a
+
 echo --------------------
 echo Git merge feature into dev
 echo --------------------
-git merge fetature_test
+git merge feature_redis
